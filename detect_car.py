b87c1b13ce65602b8e46de263ac2276f68ae0145
Anup Pradip Murumkar <87776808+AnupMurumkar@users.noreply.github.com>
2023-07-07 14:46:54 +0530

    Update detect_car.py

--- detect_car.py
+++ detect_car.py
@@ -2,26 +2,20 @@ import cv2
 import time
 import numpy as np
 
-# Create our body classifier
 car_classifier = cv2.CascadeClassifier(r'haarcascade_car.xml')  #path of haarcascade file
 
-# Initiate video capture for video file
 cap = cv2.VideoCapture(r'cars_on_highway (360p).mp4')  #path of video file
 
-
-# Loop once video is successfully loaded
 count =0
 while cap.isOpened():
     
     time.sleep(.05)
-    # Read first frame
+
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    
-    # Pass frame to our car classifier
     cars = car_classifier.detectMultiScale(gray, 1.4, 2)
     
-    # Extract bounding boxes for any bodies identified
     for (x,y,w,h) in cars:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
         count +=1
